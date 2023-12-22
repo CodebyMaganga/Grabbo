25aa4bf296f940efbb6d1e7f89c8a4a6fb2ab6c3
Maganga Mwambonu <codebymaganga@gmail.com>
2023-12-22 16:06:58 +0300

    Fixed Cart Buttons

--- Client/src/pages/components/ItemCard.js
+++ Client/src/pages/components/ItemCard.js
@@ -1,10 +1,13 @@
 "use client";
 
 import { Card } from "flowbite-react";
-import { useState, useEffect } from "react";
-import { BASE_URL } from "../../utils";
+import { useState } from "react";
+//import { useState, useEffect } from "react";
+import ITEMS from "../../Items";
+//import { BASE_URL } from "../../utils";
 
 function ItemCard({ handleClick }) {
+  /*
   const [productlist, setProductlist] = useState([]);
 
   console.log(BASE_URL);
@@ -21,85 +24,127 @@ function ItemCard({ handleClick }) {
         console.error("Error fetching data: ", error);
       });
   });
+  */
+  const [data, setData] = useState(ITEMS);
 
+  const filterItems = (category) => {
+    const result = ITEMS.filter((product) => product.category === category);
+    setData(result);
+    return result;
+  };
   return (
     <>
       <div className="bg-smokyblack text-white h-12 flex flex-row justify-center items-end mt-20">
         <h1>Fresh Produce</h1>
       </div>
+      <div className="button-container flex flex-row justify-center">
+        <div>
+          <button
+            onClick={() => setData(ITEMS)}
+            className="bg-blackbean text-white px-6 rounded-full mr-4 mt-2"
+          >
+            All
+          </button>
+        </div>
+
+        <div>
+          <button
+            onClick={() => filterItems("Vegetables")}
+            className="bg-blackbean text-white px-6 rounded-full mr-4 mt-2"
+          >
+            Vegetables
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => filterItems("Fruits")}
+            className="bg-blackbean text-white px-6 rounded-full mr-4 mt-2"
+          >
+            Fruits
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => filterItems("Animal Products")}
+            className="bg-blackbean text-white px-6 rounded-full mr-4 mt-2"
+          >
+            Animal Products
+          </button>
+        </div>
+      </div>
+
       <div className="flex flex-row flex-wrap justify-around mt-10">
-        {productlist &&
-          productlist.map((item) => (
-            <Card
-              key={item.id}
-              className="max-w-sm"
-              imgAlt="Apple Watch Series 7 in colors pink, silver, and black"
-              imgSrc={item.url}
-            >
-              <a href="#">
-                <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                  {item.name}
-                </h5>
-              </a>
-              <div className="mb-5 mt-2.5 flex items-center">
-                <svg
-                  className="h-5 w-5 text-yellow-300"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                </svg>
-                <svg
-                  className="h-5 w-5 text-yellow-300"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                </svg>
-                <svg
-                  className="h-5 w-5 text-yellow-300"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                </svg>
-                <svg
-                  className="h-5 w-5 text-yellow-300"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                </svg>
-                <svg
-                  className="h-5 w-5 text-yellow-300"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                </svg>
-                <span className="ml-3 mr-2 rounded bg-cyan-100 px-2.5 py-0.5 text-xs font-semibold text-cyan-800 dark:bg-cyan-200 dark:text-cyan-800">
-                  5.0
-                </span>
-              </div>
-              <div className="flex items-center justify-between">
-                <span className="text-3xl font-bold text-gray-900 dark:text-white">
-                  {item.price}
-                </span>
-                <button
-                  href="#"
-                  className="rounded-lg mb-2 mr-2 bg-barnRed px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
-                  onClick={() => handleClick(item)}
-                >
-                  Add to cart
-                </button>
-              </div>
-            </Card>
-          ))}
+        {data.map((item) => (
+          <Card
+            key={item.id}
+            className="max-w-sm"
+            imgAlt="Apple Watch Series 7 in colors pink, silver, and black"
+            imgSrc={item.url}
+          >
+            <a href="#">
+              <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                {item.name}
+              </h5>
+            </a>
+            <div className="mb-5 mt-2.5 flex items-center">
+              <svg
+                className="h-5 w-5 text-yellow-300"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+              </svg>
+              <svg
+                className="h-5 w-5 text-yellow-300"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+              </svg>
+              <svg
+                className="h-5 w-5 text-yellow-300"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+              </svg>
+              <svg
+                className="h-5 w-5 text-yellow-300"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+              </svg>
+              <svg
+                className="h-5 w-5 text-yellow-300"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+              </svg>
+              <span className="ml-3 mr-2 rounded bg-cyan-100 px-2.5 py-0.5 text-xs font-semibold text-cyan-800 dark:bg-cyan-200 dark:text-cyan-800">
+                5.0
+              </span>
+            </div>
+            <div className="flex items-center justify-between">
+              <span className="text-3xl font-bold text-gray-900 dark:text-white">
+                {item.price}
+              </span>
+              <button
+                href="#"
+                className="rounded-lg mb-2 mr-2 bg-barnRed px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
+                onClick={() => handleClick(item)}
+              >
+                Add to cart
+              </button>
+            </div>
+          </Card>
+        ))}
       </div>
     </>
   );
--- Client/src/pages/components/cart.js
+++ Client/src/pages/components/cart.js
@@ -10,13 +10,37 @@ const Cart = ({ cartItems, setCartItems }) => {
   };
 
   const handleAdd = (id) => {
-    console.log("add");
+    const updatedCartItems = cartItems.map((item) => {
+      if (item.id === id) {
+        if (!item.amount) {
+          item.amount = 1; // Initialize amount to 1 if it doesn't exist
+        } else {
+          item.amount += 1; // Increment the item amount by 1
+        }
+      }
+      return item;
+    });
+    setCartItems(updatedCartItems);
+  };
+
+  const handleSubtract = (id) => {
+    const updatedCartItems = cartItems.map((item) => {
+      if (item.id === id && item.amount > 1) {
+        item.amount -= 1; // Decrement the item amount by 1 if it's greater than 1
+      }
+      return item;
+    });
+    setCartItems(updatedCartItems);
   };
 
   const handlePrice = () => {
-    let ans = 0;
-    cartItems.forEach((item) => (ans += item.amount * item.price));
-    setPrice(ans);
+    let totalPrice = 0;
+    cartItems.forEach((item) => {
+      console.log("Item:", item); // Log the item to see its structure and values
+      totalPrice += item.amount * item.price; // Multiply the item price by its amount
+    });
+    console.log("Total Price:", totalPrice); // Log the total price to check its value
+    setPrice(totalPrice);
   };
 
   useEffect(() => {
@@ -26,31 +50,43 @@ const Cart = ({ cartItems, setCartItems }) => {
   return (
     <>
       <Navbar />
-      <h2 className="text-center">Cart</h2>
-      <div className="flex flex-row px-10 justify-around">
+      <h2 className="text-center">Shopping Cart</h2>
+      <div className="grid ">
         {cartItems.map((item) => (
-          <div key={item.id}>
-            <img className="h-[10rem] w-auto" src={item.url} alt={item.name} />
-            <p>{item.name}</p>
-            <p>{item.price}</p>
-            <button
-              className="bg-oldgold px-2 mr-2 rounded-full"
-              onClick={() => handleAdd()}
-            >
-              + {/* This is an example of adding an item */}
-            </button>
-            <button
-              className="bg-oldgold px-2 mr-2 rounded-full"
-              onClick={() => handleRemove(item.id)}
-            >
-              - {/* This is an example of removing an item */}
-            </button>
-            <button
-              className="bg-barnRed px-4 rounded-full text-white"
-              onClick={() => handleRemove(item.id)}
-            >
-              Remove
-            </button>
+          <div
+            className="grid grid-flow-row grid-cols-6 place-items-center my-10 border-2"
+            key={item.id}
+          >
+            <div className="flex flex-row items-center col-span-4">
+              <img
+                className="h-[10rem] w-auto"
+                src={item.url}
+                alt={item.name}
+              />
+              <p className="mx-10">{item.name}</p>
+              <p>{item.price}</p>
+            </div>
+            <div>
+              <button
+                className="bg-barnRed h-[40px] px-4 rounded-full text-white"
+                onClick={() => handleRemove(item.id)}
+              >
+                Remove
+              </button>
+              <button
+                className="bg-oldgold h-[40px] w-[30px] px-2 ml-4 rounded-full"
+                onClick={() => handleSubtract(item.id)}
+              >
+                - {/* This is an example of removing an item */}
+              </button>
+
+              <button
+                className="bg-oldgold h-[40px] w-[30px] px-2 ml-4 rounded-2xl"
+                onClick={() => handleAdd()}
+              >
+                + {/* This is an example of adding an item */}
+              </button>
+            </div>
           </div>
         ))}
       </div>
